9f8d121fa81969d0f8695fe4b39398c346d3b878
AndreaGul <andreagulino99@gmail.com>
2024-01-28 10:06:33 +0100

    feat:aggiunta delle cards con i fumetti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.main')
 
 @section('main')
-    main
-    @foreach ( $comics as $comic)
-        {{ $comic['title'] }}
-    @endforeach
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="">
+<div class="welcome-main ">
+     main
+    <h1>asdasdas</h1>
+    <div class="cards-container container">
+        <ul class="cards">
+             @foreach ( $comics as $comic)
+                <li class="card-container">
+                    <div class="img-container">
+                        <img src="{{ $comic ['thumb'] }}" alt="">
+                        <h3>{{ $comic['series'] }}</h3>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+   
+         
+        
+   
+   
+</div>
+   
 @endsection
