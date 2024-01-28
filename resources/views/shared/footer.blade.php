6049bb95404ebe6bafa25d340e1b22357d08774e
AndreaGul <andreagulino99@gmail.com>
2024-01-28 09:47:11 +0100

    feat: suddivisione della pagina e inserimento dei dati tramite un file in config

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>dc comics</title>
+  @vite('resources/js/app.js')
+</head>
+<body>
+  <h2>footer</h2>
+  {{-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt=""> --}}
+</body>
+</html>
